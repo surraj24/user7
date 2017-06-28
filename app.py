4093381cd74c2425ad685acfbb0ab245752eb719
Suraj Agrawal <surajagrawal24@gmail.com>
2017-06-28 15:03:27 +0530

    fifth

--- app.py
+++ app.py
@@ -28,8 +28,11 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") == "shipping.cost":
+    if req.get("result").get("action") != "shipping.cost":
         return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("shipping-zone")
     
     speech = "The cost of shipping to "
 
